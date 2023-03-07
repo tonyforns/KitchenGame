51701119efac41b6465777d877e40db10b65f786
Tony Forns <>
2023-03-07 15:26:52 +1100

    Selected counter visual

---
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedCounterVisual : MonoBehaviour
+{
+
+    [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualObject;
+    private void Start()
+    {
+        Player.Instance.OnSelectedCounterChange += Player_OnSelectedCounterChange; 
+    }
+
+    private void Player_OnSelectedCounterChange(object sender, Player.OnSelectedCounterChangeEventArgs e)
+    {
+       if(e.selectedCounter == clearCounter)
+        {
+            Show();
+        } else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        visualObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        visualObject.SetActive(false);
+    }
+}
