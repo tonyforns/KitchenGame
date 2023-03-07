68d7227b8856578013dfd0fcf5835c0db9a4e8d3
Tony Forns <>
2023-03-07 12:44:49 +1100

    Add postrendering config - Add animation controller - Add GameInput manager - Add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private float movSpeed = 7;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += movDir * Time.deltaTime * movSpeed;
+
+        isWalking = movDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
