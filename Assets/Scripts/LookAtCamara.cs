e6d2de9ac8366ec18ac35559d40a23b3565e8a42
TONY\iafor <TonyForns>
2023-05-27 17:19:01 +1000

    Add trash counter + ReOrder Scene

---
+++ Assets/Scripts/LookAtCamara.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LookAtCamara : MonoBehaviour
+{
+
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    [SerializeField] private Mode mode;
+    private void LateUpdate()
+    {
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 dirFromCamara = transform.position - Camera.main.transform.position;
+                transform.LookAt(dirFromCamara + transform.position);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+            default:
+                break;
+        }
+    }
+}
