1ef47d4c0783468ce522d2cdd6b14d27fadece33
TONY\iafor <TonyForns>
2023-06-03 16:13:49 +1000

    Add binding key functionality

--- Assets/Scripts/OptionsUI.cs
+++ Assets/Scripts/OptionsUI.cs
@@ -14,6 +14,22 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
 
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAltButton;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAltText;
+    [SerializeField] private TextMeshProUGUI pauseText;
+    [SerializeField] private Transform pressToRebindKeyTransform;
+
     private void Awake()
     {
         Instance = this;
@@ -40,6 +56,7 @@ public class OptionsUI : MonoBehaviour
 
     private void KitchenGameManager_OnGameUnpaused(object sender, System.EventArgs e)
     {
+        HidePressToRebindKey();
         Hide();
     }
 
@@ -47,6 +64,22 @@ public class OptionsUI : MonoBehaviour
     {
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+
+        moveUpText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Move_Up);
+        moveDownText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Move_Down);
+        moveLeftText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Move_Left);
+        moveRightText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Move_Right);
+        interactText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Interact);
+        interactAltText.text = GameInput.Instance.GetBiningText(GameInput.Binding.InteractAlternate);
+        pauseText.text = GameInput.Instance.GetBiningText(GameInput.Binding.Pause);
+
+        moveUpButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Move_Up));
+        moveDownButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Move_Down));
+        moveLeftButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Move_Left));
+        moveRightButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Move_Right));
+        interactButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Interact));
+        interactAltButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.InteractAlternate));
+        pauseButton.onClick.AddListener(() => RebindBinding(GameInput.Binding.Pause));
     }
 
     public void Show()
@@ -57,5 +90,23 @@ public class OptionsUI : MonoBehaviour
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    public void ShowPressToRebindKey()
+    {
+        pressToRebindKeyTransform.gameObject.SetActive(true);
+    }
+
+    public void HidePressToRebindKey()
+    {
+        pressToRebindKeyTransform.gameObject.SetActive(false);
+    }
+
+    public void RebindBinding(GameInput.Binding binding)
+    {
+        ShowPressToRebindKey();
+        GameInput.Instance.RebindBinding(binding, () => 
+        HidePressToRebindKey());
+        UpdateVisual(); ;
     }
 }
